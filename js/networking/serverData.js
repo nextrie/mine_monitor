0237e1dfd6bc71cf308a269080e62cad98cf9d10
Alexandre Gerez <nextriesh@gmail.com>
2020-01-08 23:43:24 +0100

    quick fix

--- js/networking/serverData.js
+++ js/networking/serverData.js
@@ -6,12 +6,11 @@ async function getServerData(serverIP)
 {
     var serverData;
     var apiURL = "https://api.mcsrvstat.us/2/" + serverIP;
-    
+
     await $.getJSON(apiURL, (data) => {
         serverData = data;
     });
-    
-    console.log(serverData);
+
     return serverData;
 }
 
@@ -28,7 +27,6 @@ async function displayServerData(serverData)
         $("#serverIP").css("color", "red");
     else
         $("#serverIP").css("color", "rgb(20, 255, 129)");
-
     $("#serverMOTD").html(serverData["motd"]["html"]);
     $("#playerCountDisplay").text("Players : " + serverData["players"]["online"] + " / " + serverData["players"]["max"]);
     updatePlayerCountDisplay(serverData["players"]["online"]);
